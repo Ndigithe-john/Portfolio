411b0798473ecec1cdf32365f2f52dc1b91b84a5
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-15 16:24:47 +0300

    updated portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -75,7 +75,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/Ndigithe-john/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
